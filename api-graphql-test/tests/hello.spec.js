c1ec02a73bcb93e7be30d91dc4a030e9ab9e5879
conanchen <conan8chan@yahoo.com>
2018-04-16 08:53:53 +0800

    api graphql testing with jest

--- api-graphql-test/tests/hello.spec.js
+++ api-graphql-test/tests/hello.spec.js
@@ -1,37 +1,62 @@
 import axios from 'axios';
-import { XMLHttpRequest } from 'xmlhttprequest';
+import {XMLHttpRequest} from 'xmlhttprequest';
 
 global.XMLHttpRequest = XMLHttpRequest;
 
 describe('hellos', () => {
-  test('hellos', async () => {
-    const response = await axios.post('http://localhost:9999/graphql', {
-      query: `
+    test('hellos', async() =>{
+    const response = await
+    axios.post('http://localhost:9999/graphql', {
+        query: `
         {
           hellos(after: "cursor") {
             edges {
               cursor
-              node {
-                id
-                message
-                create_time
-                update_time
-              }
             }
           }
         }
       `,
     });
 
-    const { data } = response;
-    // expect(data).toMatchObject({
-    //   data: {
-    //     hellos: [],
-    //   },
-    // });
-    expect(Object.keys(data)).toEqual(
-        expect.arrayContaining(["name", "lastname", "role", "age"])
-    );
-  });
+    expect(response.status).toEqual(200);
+
+    const {data} = response;
+    expect(data).toMatchObject({
+        data: {
+            hellos: {
+                edges: [{
+                    "cursor": "aGVsbG9zLzlkYmVhNDIzLWU4NWQtNGY0NC1hMjZiLWIyNzEyOTVhZGNlOA=="
+                },{
+                    "cursor": "cursor=="
+                },{
+                    "cursor": "cursor"
+                },{
+                    "cursor": "cursor"
+                },{
+                    "cursor": "cursor"
+                },{
+                    "cursor": "cursor"
+                },{
+                    "cursor": "cursor"
+                },{
+                    "cursor": "cursor"
+                },{
+                    "cursor": "cursor"
+                },{
+                    "cursor": "cursor"
+                },{
+                    "cursor": "cursor"
+                },{
+                    "cursor": "cursor"
+                },{
+                    "cursor": "aGVsbG9zLzQzMDBkOTVlLTExZmYtNGZmOS05NWI3LWNmNTQxOWEyMDc3MA=="
+                },]
+            },
+        },
+    });
+    // expect(Object.keys(data.edges)).toEqual(
+    //     expect.arrayContaining(["name", "lastname", "role", "age"])
+    // );
+    });
 
 });
