2efcc0ead134d83b2fea119bbeff0e238832d332
hai <hilin2333@gmail.com>
2018-07-20 15:27:31 +0800

    日志测试

--- gateway-hello-graphql/src/main/java/guoi/hello/HelloGatewayApplication.java
+++ gateway-hello-graphql/src/main/java/guoi/hello/HelloGatewayApplication.java
@@ -3,6 +3,8 @@ package guoi.hello;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class HelloGatewayApplication {
@@ -10,5 +12,8 @@ public class HelloGatewayApplication {
     public static void main(String[] args) {
         SpringApplication.run(HelloGatewayApplication.class, args);
     }
-
+    @Bean
+    public RestTemplate restTemplate(){
+        return new RestTemplate();
+    }
 }
---
+++ gateway-hello-graphql/src/main/java/guoi/hello/LogRunner.java
@@ -0,0 +1,57 @@
+package guoi.hello;
+
+import lombok.extern.log4j.Log4j;
+import lombok.extern.log4j.Log4j2;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.logging.log4j.Level;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Map;
+import java.util.Random;
+
+
+/**
+ * @author hai
+ * description
+ * email hilin2333@gmail.com
+ * date 2018/7/20 2:54 PM
+ */
+@Component
+@Log4j2
+public class LogRunner implements CommandLineRunner {
+
+    private static Random random = new Random();
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @Override
+    public void run(String... args) throws Exception {
+            while (true) {
+                log.log(randomLevel(), randomMessage());
+                Thread.sleep(3000);
+
+            }
+    }
+
+    private Level randomLevel() {
+        switch (random.nextInt(3)) {
+            case 0:
+                return Level.DEBUG;
+            case 1:
+                return Level.INFO;
+            case 2:
+                return Level.ERROR;
+            default:
+                return Level.INFO;
+        }
+    }
+
+    private String randomMessage() {
+       Map  map = restTemplate.getForObject("http://api.icndb.com/jokes/random", Map.class,new Object());
+        Map map1 = (Map)map.get("value");
+       return map1.get("joke").toString();
+    }
+}
